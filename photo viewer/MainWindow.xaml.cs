5cf4ce08f0f985633293db9fe57a4392883cc372
Jacob Conner <jconn025@odu.edu>
2022-11-01 13:47:12 -0400

    first commit

---
+++ photo viewer/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.Storage.Search;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace photo_viewer
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            GetItemsAsync();
+        }
+
+        public ObservableCollection<ImageFileInfo> Images { get; } = new ObservableCollection<ImageFileInfo>();
+
+        private async Task GetItemsAsync()
+        {
+            StorageFolder appInstalledFolder = Package.Current.InstalledLocation;
+            StorageFolder picturesFolder = await appInstalledFolder.GetFolderAsync("Assets\\Samples");
+
+            var result = picturesFolder.CreateFileQueryWithOptions(new QueryOptions());
+
+            IReadOnlyList<StorageFile> imageFiles = await result.GetFilesAsync();
+            foreach (StorageFile file in imageFiles)
+            {
+                Images.Add(await LoadImageInfo(file));
+            }
+
+        }
+
+        public async static Task<ImageFileInfo> LoadImageInfo(StorageFile file)
+        {
+            var properties = await file.Properties.GetImagePropertiesAsync();
+            ImageFileInfo info = new(properties, file, file.DisplayName, file.DisplayType);
+
+            return info;
+        }
+
+        private void ImageGridView_ContainerContentChanging(
+            ListViewBase sender,
+            ContainerContentChangingEventArgs args)
+        {
+            if (args.InRecycleQueue)
+            {
+                var templateRoot = args.ItemContainer.ContentTemplateRoot as Grid;
+                var image = templateRoot.FindName("ItemImage") as Image;
+                image.Source = null;
+            }
+
+            if(args.Phase == 0)
+            {
+                args.RegisterUpdateCallback(ShowImage);
+                args.Handled = true;
+            }
+        }
+
+        private async void ShowImage(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if(args.Phase == 1)
+            {
+                //it's phase 1 so show this item's image
+                var templateRoot = args.ItemContainer.ContentTemplateRoot as Grid;
+                var image = templateRoot.FindName("ItemImage") as Image;
+                var item = args.Item as ImageFileInfo;
+                image.Source = await item.GetImageThumbnailAsync(); 
+            }
+        }
+    }
+}
